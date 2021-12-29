7011859b0971a662e354deb001c7dff5f6168e1a
zendagin <dicky1126277766238@gmail.com>
2021-12-30 00:14:02 +0900

    x

---
+++ components/TopHeader.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <div>
+    <div class="container mx-auto flex pt-5 pb-4 items-center justify-between">
+      <div>left</div>
+      <div>right</div>
+    </div>
+    <div id="nav" class="container">nav</div>
+  </div>
+</template>
+
+<script setup>
+</script>
