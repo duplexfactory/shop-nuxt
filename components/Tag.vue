fcb8bf4e85778621ef82336b0dd95807129c5f74
Victor Kwok <victorkwok97@gmail.com>
2021-12-30 19:33:34 +0800

    cards

---
+++ components/Tag.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps({
+  title: String
+})
+</script>
+
+<template>
+  <div class="inline text-pink-400 text-xs border border-pink-400 px-2 py-1 rounded-md">
+    {{ title }}
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@ const { counter } = goSick();
 
 const storeName = 'caseonlyy';
 
-const res = await useFetch(`/api/shop?id=${storeName}`);
-console.log(res.data.value);
+// const res = await useFetch(`/api/shop?id=${storeName}`);
+// console.log(res.data.value);
 
 // let res = await fetch(`https://www.instagram.com/${storeName}/?__a=1`);
 // res = await res.text();
@@ -36,26 +36,80 @@ const description = 'description'; // Dummy
 
 
     <div class="container mx-auto">
-      <div>
-        Store Card Square
-        <div>
+      <div>熱門店鋪</div>
+      <div>最新產品</div>
+      <div>活躍店長</div>
+      <div>實體店鋪</div>
+    </div>
 
-          <div class="flex flex-row border">
-            <div class="mr-4 bg-gray-500 rounded">
-              Image Here
+
+    <div class="container mx-auto pb-20">
+
+      <div class="mt-10 text-xl">Components Showcase</div>
+
+      <div class="mt-8 mb-4">Simple Store Card</div>
+      <div class="flex flex-row">
+        <div class="p-4 text-center">
+          <div class="bg-gray-300 rounded-full mx-auto" style="aspect-ratio: 1; height: 100px;">
+          </div>
+
+          <div class="mt-2 font-semibold text-lg">{{ storeName }}</div>
+          <div class="mt-2 flex">
+            <tag v-for="i in [1, 2, 3]"
+                 :key="i.toString()"
+                 class="mr-2"
+                 title="Tag 1"></tag>
+          </div>
+          <button class="mt-4 text-white text-md bg-pink-400 px-6 py-2 rounded-md">探索</button>
+
+        </div>
+      </div>
+
+      <div class="mt-8 mb-4">Store Card Square</div>
+      <div class="flex flex-row">
+
+        <div class="border rounded-md grid grid-cols-2">
+          <div class="p-4 col-span-1">
+            <div class="bg-gray-300 rounded-full" style="aspect-ratio: 1; height: 80px;">
+
             </div>
-            <div>
-              <div class="font-semibold text-lg">Store name</div>
-              <div class="mt-1 text-gray-500">Followers 1,000</div>
-              <div class="mt-2 flex flex-row">
-                <div v-for="i in [1, 2, 3]" :key="i.toString()" class="text-white text-sm bg-pink-400 px-2 py-1 rounded-lg mr-2">Tag 1</div>
+
+            <div class="mt-2 font-semibold text-lg">{{ storeName }}</div>
+
+            <div class="mt-2 text-sm text-gray-500 flex flex-row">
+              <div class="text-center" style="flex: 1;">
+                <div>Followers</div>
+                <div>1,000</div>
               </div>
+              <div class="bg-gray-300 mx-2" style="width: 1px;"></div>
+              <div class="text-center" style="flex: 1;">
+                <div>Posts</div>
+                <div>1,000</div>
+              </div>
+            </div>
+
+            <div class="mt-4 flex">
+              <tag v-for="i in [1, 2, 3]"
+                   :key="i.toString()"
+                   class="mr-2"
+                   title="Tag 1"></tag>
+            </div>
+            <div class="mt-2 text-gray-500 text-xs">Last active {{ lastActive }}</div>
+
+          </div>
+
+          <div class="col-span-1">
+            <div class="bg-gray-300" style="aspect-ratio: 1; height: 200px;"></div>
+            <div class="flex flex-row">
+              <div class="bg-red-300" style="aspect-ratio: 1; height: 100px;"></div>
+              <div class="bg-green-300" style="aspect-ratio: 1; height: 100px;"></div>
             </div>
           </div>
         </div>
+      </div>
 
-        Store Card Rectangle
-        <div class="border rounded-xl p-4 flex flex-row">
+      <div class="mt-8 mb-4">Store Card Rectangle</div>
+      <div class="border rounded-md p-4 flex flex-row">
           <div class="mr-4">
             <div class="bg-gray-300 rounded-full" style="aspect-ratio: 1; height: 80px;">
 
@@ -75,12 +129,11 @@ const description = 'description'; // Dummy
               </div>
             </div>
 
-            <div class="mt-4">
-              <div v-for="i in [1, 2, 3]"
+            <div class="mt-4 flex">
+              <tag v-for="i in [1, 2, 3]"
                    :key="i.toString()"
-                   class="inline text-white text-xs bg-pink-400 px-2 py-1 rounded-lg mr-2">
-                Tag 1
-              </div>
+                   class="mr-2"
+                   title="Tag 1"></tag>
             </div>
             <div class="mt-2 text-gray-500 text-xs">Last active {{ lastActive }}</div>
 
@@ -95,7 +148,7 @@ const description = 'description'; // Dummy
 
         </div>
 
-      </div>
+
     </div>
 
 
