c1f78aaf737b70768cf1994bda16eb9d776626cc
Victor Kwok <victorkwok97@gmail.com>
2022-01-10 19:20:43 +0800

    Nav bar WIP

--- components/TopHeader.vue
+++ components/TopHeader.vue
@@ -1,12 +1,20 @@
 
 <template>
   <div>
-    <div class="container mx-auto flex pt-5 pb-4 items-center justify-between">
-      <div>left</div>
+    <div class="container mx-auto pt-5 pb-4 flex items-center justify-between">
+      <!-- left -->
+      <div class="flex items-center">
+        <div class="mr-4">LOGO HERE</div>
+        <input class="border py-2 px-4 text-md" style="min-width: 300px;" placeholder="商店 或 產品 名稱" type="search" autocomplete="off" id="search" name="search" />
+        <button class="text-white text-md bg-pink-400 px-6 py-2">搜尋</button>
+      </div>
+
+      <!-- right -->
       <div>right</div>
     </div>
     <div id="nav" class="container">nav</div>
   </div>
+
 </template>
 
 <script setup>
