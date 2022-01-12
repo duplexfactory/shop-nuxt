d8ef32e0dcdbbd6897906ded897c55631b0dcdab
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-01-13 00:54:52 +0800

    stick button to card bottom

--- components/StoreCardSquare.vue
+++ components/StoreCardSquare.vue
@@ -24,39 +24,38 @@
 <template>
     <div class="border rounded-md grid grid-cols-2">
         <div class="p-4 col-span-1">
+            <div class="relative h-full">
+                <div class="flex flex-row items-center">
+                    <div class="bg-gray-300 rounded-full square-image-container" :style="`background-image: url(${profilePicUrl});`" style="height: 60px;"></div>
 
-            <div class="flex flex-row items-center">
-                <div class="bg-gray-300 rounded-full square-image-container" :style="`background-image: url(${profilePicUrl});`" style="height: 60px;"></div>
-
-                <div class="ml-2 overflow-hidden">
-                    <div class="font-semibold text-lg truncate">{{ username }}</div>
-                    <div class="text-gray-400 font-light text-xs">最後活躍 {{ lastActive }}</div>
+                    <div class="ml-2 overflow-hidden">
+                        <div class="font-semibold text-lg truncate">{{ username }}</div>
+                        <div class="text-gray-400 font-light text-xs">最後活躍 {{ lastActive }}</div>
+                    </div>
                 </div>
-            </div>
 
-
-            <div class="mt-2 2xl:mt-4 text-sm text-gray-500 flex flex-row">
-                <div class="text-center" style="flex: 1;">
-                    <div>粉絲</div>
-                    <div>{{ followerCount }}</div>
+                <div class="mt-2 2xl:mt-4 text-sm text-gray-500 flex flex-row">
+                    <div class="text-center" style="flex: 1;">
+                        <div>粉絲</div>
+                        <div>{{ followerCount }}</div>
+                    </div>
+                    <div class="bg-gray-300 mx-2" style="width: 1px;"></div>
+                    <div class="text-center" style="flex: 1;">
+                        <div>貼文</div>
+                        <div>{{ mediaCount }}</div>
+                    </div>
                 </div>
-                <div class="bg-gray-300 mx-2" style="width: 1px;"></div>
-                <div class="text-center" style="flex: 1;">
-                    <div>貼文</div>
-                    <div>{{ mediaCount }}</div>
+
+                <div class="mt-1 2xl:mt-2"
+                     style="overflow: hidden; display: -webkit-box; -webkit-line-clamp: 2; /* number of lines to show */ -webkit-box-orient: vertical; font-size: 0;">
+                    <tag v-for="tag in tags"
+                         :key="tag"
+                         class="mr-1 2xl:mr-2 mt-1 2xl:mt-2 px-1 py-0.5 2xl:px-2 2xl:py-1"
+                         :title="tagsLookup[tag]"></tag>
                 </div>
+
+                <button class="mt-2 2xl:mt-4 btn-primary absolute" style="bottom: 0px;">進入店鋪</button>
             </div>
-
-            <div class="mt-1 2xl:mt-2"
-                 style="overflow: hidden; display: -webkit-box; -webkit-line-clamp: 2; /* number of lines to show */ -webkit-box-orient: vertical; font-size: 0;">
-                <tag v-for="tag in tags"
-                     :key="tag"
-                     class="mr-1 2xl:mr-2 mt-1 2xl:mt-2 px-1 py-0.5 2xl:px-2 2xl:py-1"
-                     :title="tagsLookup[tag]"></tag>
-            </div>
-
-            <button class="mt-2 2xl:mt-4 btn-primary">進入店鋪</button>
-
         </div>
 
         <div class="col-span-1">
