8428a028e29e88bba445cb4a085c30d3180bdd05
Victor Kwok <victorkwok97@gmail.com>
2022-01-14 11:46:06 +0800

    more responsive header

--- components/TopHeader.vue
+++ components/TopHeader.vue
@@ -3,14 +3,14 @@
   <div>
     <div class="container mx-auto pt-5 pb-4 flex items-center justify-between">
       <!-- left -->
-      <div class="flex items-center">
+      <div class="flex items-center flex-1">
         <div class="mr-4">LOGO HERE</div>
-        <input class="border py-2 px-4 text-md" style="min-width: 300px;" placeholder="商店 或 產品 名稱" type="search" autocomplete="off" id="search" name="search" />
-        <button class="border border-pink-400 text-white text-md bg-pink-400 px-6 py-2">搜尋</button>
+        <input class="border py-2 px-4 text-md flex-1" style="max-width: 300px;" placeholder="商店 或 產品 名稱" type="search" autocomplete="off" id="search" name="search" />
+        <button class="border border-pink-400 btn-primary !rounded-none">搜尋</button>
       </div>
 
       <!-- right -->
-      <div>
+      <div class="hidden md:block">
         <button class="btn-outline">認證我的商店</button>
       </div>
     </div>
