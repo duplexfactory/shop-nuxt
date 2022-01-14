de70044d5f412d8f3d28eadc50159c25ba22fac1
zendagin <dicky1126277766238@gmail.com>
2022-01-14 18:20:49 +0900

    ﻿editorjs
    yarn lock

---
+++ pages/ed/read.vue
@@ -0,0 +1,28 @@
+<script setup>
+    // const c = await useAsyncData('count', () => $fetch('/api/lv1/count'), { pick: ['title', 'count'] })
+    const [{data}, count, cols] = await Promise.all([
+        useFetch('/api/hello'),
+        useFetch('/api/lv1/count', {pick: ['title', 'count']}).then(
+            ({data}) => data.value.count
+        ),
+        useFetch('/api/get'),
+    ])
+
+    const {counter, inc, dec} = goSick()
+</script>
+
+<template>
+    <div>
+        <p>{{ data.message }}</p>
+        <p>{{ cols }}</p>
+        <p>Global count with API: {{ count || 'x' }}</p>
+
+        <div>
+            Local Count with Composables: {{ counter }}
+            <button @click="inc">+</button>
+            <button @click="dec">-</button>
+        </div>
+
+        <nuxt-link to='/'>back</nuxt-link>
+    </div>
+</template>
