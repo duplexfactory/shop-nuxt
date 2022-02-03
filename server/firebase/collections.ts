f4b1da4f4b352ebe606d08ae5f69b3ab429a924c
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-02-04 00:23:19 +0800

    review collection

--- server/firebase/collections.ts
+++ server/firebase/collections.ts
@@ -4,12 +4,17 @@ import IgMedia from "~/models/IgMedia";
 import IgTray from "~/models/IgTray";
 import IgStory from "~/models/IgStory";
 import {initFirebase} from "~/server/firebase/init";
+import IgPageReview from "~/models/IgPageReview";
 // import {firestore} from "firebase-admin/firestore";
 // import CollectionGroup = firestore.CollectionGroup;
 initFirebase();
 
 export function pageCollection() {
     return getFirestore().collection("pages") as CollectionReference<IgPage>;
+}
+
+export function reviewCollection() {
+    return getFirestore().collection("reviews") as CollectionReference<IgPageReview>;
 }
 
 export type IdInput = number | string | { id: string | number }
