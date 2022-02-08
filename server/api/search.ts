86a4222f5f4df3dc7ca3ea0580b704ef76d2b722
zendagin <dicky1126277766238@gmail.com>
2022-02-08 20:03:30 +0900

    search api

---
+++ server/api/search.ts
@@ -0,0 +1,39 @@
+import {IncomingMessage, ServerResponse} from "http";
+import {useQuery} from 'h3';
+import {initMongo, pageSearchCollection} from "~/server/mongodb";
+import {PageSearch} from "~/models/PageSearch";
+import {Filter} from "mongodb";
+
+function escapeRegExp(string: string): string {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+}
+
+export default async function (req: IncomingMessage, res: ServerResponse): Promise<{ pages: PageSearch[], count: number }> {
+    const {
+        q,
+        tag,
+        br,
+        phy,
+        skip,
+        limit
+    } = await useQuery(req) as { q?: string, tag?: string, br?: string, phy?: string, skip: string, limit: string };
+
+    const f: Filter<PageSearch> = q ? {
+        $or: [
+            {username: {$regex: escapeRegExp(q), $options: "i"}},
+            {fullName: {$regex: escapeRegExp(q), $options: "i"}},
+            {biography: {$regex: escapeRegExp(q), $options: "i"}},
+        ]
+    } : {};
+
+    if (tag) f.tags = tag;
+    if (br) f.businessRegistration = true;
+    if (phy) f.brickAndMortar = true;
+
+    await initMongo();
+
+    return {
+        pages: await pageSearchCollection.find(f).sort({activeScore: -1}).skip(Number(skip) || 0).limit(Number(limit) || 0).toArray(),
+        count: await pageSearchCollection.countDocuments(f)
+    }
+}
