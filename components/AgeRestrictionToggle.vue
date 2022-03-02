f2feb2afd44199db82ed8bd1d9e204d56c4f3098
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-03-02 21:43:33 +0800

    Toggle colour and width

--- components/AgeRestrictionToggle.vue
+++ components/AgeRestrictionToggle.vue
@@ -5,8 +5,8 @@
           @change="showAgeRestrictedContentBufferChanged"
           :classes="{
             container: 'inline-block rounded-full outline-none focus:ring focus:ring-green-500 focus:ring-opacity-30',
-            toggle: 'flex w-12 h-5 rounded-full relative cursor-pointer transition items-center box-content border-2 text-xs leading-none',
-            toggleOn: 'bg-green-500 border-green-500 justify-start text-white',
+            toggle: 'flex w-16 h-5 rounded-full relative cursor-pointer transition items-center box-content border-2 text-xs leading-none',
+            toggleOn: 'bg-red-500 border-red-500 justify-start text-white',
             toggleOff: 'bg-gray-200 border-gray-200 justify-end text-gray-700',
             toggleOnDisabled: 'bg-gray-300 border-gray-300 justify-start text-gray-400 cursor-not-allowed',
             toggleOffDisabled: 'bg-gray-200 border-gray-200 justify-end text-gray-400 cursor-not-allowed',
@@ -15,7 +15,7 @@
             handleOff: 'left-0',
             handleOnDisabled: 'bg-gray-100 left-full transform -translate-x-full',
             handleOffDisabled: 'bg-gray-100 left-0',
-            label: 'text-center w-8 border-box whitespace-nowrap select-none',
+            label: 'text-center w-12 border-box whitespace-nowrap select-none',
           }" />
 </template>
 
