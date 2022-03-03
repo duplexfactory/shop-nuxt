1eddf1e061458ddd48a84bd6229821c7a0dbae63
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-03-03 22:49:30 +0800

    home api adult filter

--- server/api/home.ts
+++ server/api/home.ts
@@ -1,6 +1,9 @@
 import {pageCollection} from "~/server/firebase/collections";
 import IgPage from "~/models/IgPage";
 import {fields} from "~/server/firebase/init";
+import {IncomingMessage, ServerResponse} from "http";
+import {useQuery} from "h3";
+import {Query, DocumentData} from "@google-cloud/firestore";
 
 const cardFields= [
     "pk",
@@ -16,33 +19,39 @@ const cardFields= [
     "locations"
 ] as const
 
-export default async () => {
+export default async (req: IncomingMessage, res: ServerResponse) => {
+
+    const {
+        adult
+    } = await useQuery(req) as { adult: string };
+
+    let q: Query<DocumentData> = pageCollection()
+        .where("deleted", "==", false)
+        .where("private", "==", false);
+    if (adult !== "true") {
+        q = q.where("adult", "==", false)
+    }
+
     const [hot, latest, active, physical] = await Promise.all([
-        pageCollection()
-            .where("deleted", "==", false)
-            .where("private", "==", false)
+        q
             .orderBy("followerCount", "desc")
             .limit(21)
             .pick(...cardFields)
             .get()
             .then(ss => ss.data()),
-        pageCollection()
+        q
             .orderBy("lastMedia", "desc")
             .limit(12)
             .pick("lastMediaData", "pk", "username", "fullName")
             .get()
             .then(ss => ss.data()),
-        pageCollection()
-            .where("deleted", "==", false)
-            .where("private", "==", false)
+        q
             .orderBy("activeScore", "desc")
             .limit(30)
             .pick(...cardFields)
             .get()
             .then(ss => ss.data()),
-        pageCollection()
-            .where("deleted", "==", false)
-            .where("private", "==", false)
+        q
             .where("brickAndMortar", "==", true)
             .orderBy("activeScore", "desc")
             .limit(21)
