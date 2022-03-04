9c622a5aeae52f812eb068eeec76e4dd968f9bc7
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-03-04 19:05:24 +0800

    nuxt config meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,31 @@ export default defineNuxtConfig({
     "@vueuse/nuxt",
     "nuxt-windicss",
   ],
+  meta: {
+    title: 'IG Shop 推薦及評論平台 | IG Shop 搜尋器丨Shopitout',
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0'},
+      {name: 'theme-color', content: '#f472b6'},
+      {name: 'keywords', content: 'Shopitout, ig shop, ig shop衫, instagram, 網店, ig shop商業登記, ig shop推薦, ig shop攻略'},
+      {property: 'og:locale', hid: 'og:locale', content: 'zh_HK'},
+      {property: 'og:title', hid: 'og:title', content: 'IG Shop 推薦及評論平台 | IG Shop 搜尋器丨Shopitout'},
+      // {property: 'og:url', hid: 'og:url', content: process.env.DOMAIN_WEB},
+      // {
+      //   property: 'og:image',
+      //   hid: 'og:image',
+      //   content: `${process.env.DOMAIN_WEB}/assets/img/common/home_preview.jpg`
+      // },
+      // {property: 'og:image:width', hid: 'og:image:width', content: '960'},
+      // {property: 'og:image:height', hid: 'og:image:height', content: '540'},
+      {
+        property: 'og:description',
+        hid: 'og:description',
+        content: 'Shopitout是一個IG Shop評論平台及搜尋器，整理及分類香港的IG Shop，令買家輕鬆搜尋、瀏覽、發掘大眾推薦的IG Shop，立即了解！'
+      },
+      {property: 'og:site_name', hid: 'og:site_name', content: 'Shopitout'}
+    ],
+  },
   windicss: {
     analyze: true
   },
