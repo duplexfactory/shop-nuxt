9d740b9604367171e070446a280fa1de57d19525
zendagin <dicky1126277766238@gmail.com>
2022-03-08 19:14:01 +0900

    get media by username

--- server/api/media.ts
+++ server/api/media.ts
@@ -2,12 +2,22 @@ import type {IncomingMessage, ServerResponse} from 'http'
 import {useQuery} from 'h3'
 import type IgMedia from "~/models/IgMedia";
 import {getPageMedias, initDynamo} from "~/server/dynamodb";
+import {initMongo, pageSearchCollection} from "~/server/mongodb";
 
-// example id 2305673031
 export default async function (req: IncomingMessage, res: ServerResponse): Promise<{ medias: IgMedia[] }> {
-    const {id} = await useQuery(req) as { id: string }
+    let {id, username} = await useQuery(req) as { id: string, username: string }
+
+    let queryId: number
+    if (id) {
+        queryId = Number(id)
+    } else {
+        await initMongo();
+        const p = await pageSearchCollection.findOne({username}, {projection: {_id: 1}});
+        queryId = p._id
+    }
+
     initDynamo();
     return {
-        medias: await getPageMedias(Number(id))
+        medias: await getPageMedias(queryId)
     }
 }
