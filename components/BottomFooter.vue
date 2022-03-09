a30b8fae97c399b1cb1f38d55d8a0dc928f17753
zendagin <dicky1126277766238@gmail.com>
2022-03-09 20:22:34 +0900

    take down page and footer

--- components/BottomFooter.vue
+++ components/BottomFooter.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="bg-gray-100 text-sm text-gray-600">
-    <div class="container mx-auto py-4">
+    <div class="bg-gray-100 text-sm text-gray-600">
+        <div class="container mx-auto py-4">
 
-      <div class="flex items-center">
-        <nuxt-link to="/verify" class="mr-6">認證我的商店</nuxt-link>
-        <div class="mr-6">提供商店資料</div>
-        <div class="mr-6">關於我們</div>
-      </div>
+            <div class="flex items-center">
+<!--                <nuxt-link to="/verify" class="mr-6">認證我的商店</nuxt-link>-->
+<!--                <div class="mr-6">提供商店資料</div>-->
+                <nuxt-link to="/takedown" class="mr-6">移除商店資料</nuxt-link>
+<!--                <div class="mr-6">關於我們</div>-->
+                <div class="mr-6">私隱條款</div>
+            </div>
 
-      <hr class="my-2"/>
+            <hr class="my-2"/>
 
-      <div class="text-right">
-        © 2022 InstaShop. All Rights Reserved.
-      </div>
+            <div class="text-right">
+                © 2022 InstaShop. All Rights Reserved.
+            </div>
 
+        </div>
     </div>
-  </div>
-
 </template>
 
---
+++ pages/takedown.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mb-16">
+        <div class="container mx-auto">
+            <div class="mx-auto leading-6" style="max-width: 800px">
+                <div class="text-2xl mt-10">移除商店資料</div>
+                <div class="text-gray-600 text-xl mt-6">
+                    <div>如欲從Shopitout移除您的商店資料，請於Instagram以相同帳戶私訊 <a href="https://www.instagram.com/xxxx/">＠xxxx</a> 。</div>
+                    <div class="mt-4">在確認身份後，您的商店資料將會於數日內移除。</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
