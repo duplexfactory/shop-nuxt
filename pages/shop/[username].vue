059e99e32b899abb9e0d7f3ea04f1b1fd4a7db80
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-03-11 14:19:03 +0800

    lazy card

--- pages/shop/[username].vue
+++ pages/shop/[username].vue
@@ -188,13 +188,12 @@ export default  {
                     <h2 class="px-5 py-2 md:px-6 md:py-3 cursor-pointer" :class="{'tab-selected': selectedIndex == 1}" @click="selectedIndex = 1; fetchReviews();">評論</h2>
                 </div>
                 <div v-if="selectedIndex == 0" class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-4">
-                  <MediaCard v-for="media in medias"
-                             class="col-span-1"
-                             @click="showMediaModal(media.code)"
-                             style="cursor: pointer"
-                             :media="media"
-                             :shop="page"
-                             :key="media.id + '-post-card'"></MediaCard>
+                  <lazy-component v-for="media in medias" :key="media.id + '-post-card'" class="col-span-1">
+                    <MediaCard @click="showMediaModal(media.code)"
+                               style="cursor: pointer"
+                               :media="media"
+                               :shop="page"></MediaCard>
+                  </lazy-component>
                 </div>
                 <div v-else class="grid grid-cols-1 lg:grid-cols-2 gap-x-8">
                   <div class="col-span-1 lg:order-2">
