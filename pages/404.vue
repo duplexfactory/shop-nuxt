9e3c2fe470bf86d1a0ee95834698753fcc4dd783
zendagin <dicky1126277766238@gmail.com>
2022-03-17 17:11:11 +0900

    404

---
+++ pages/404.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container mx-auto self-center pb-28">
+        <div class="text-pink-500 text-8xl font-bold w-full text-center mb-4">
+            404
+        </div>
+        <div class="text-gray-600 text-xl w-full text-center">
+            頁面不存在或已被移除
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        error: Object
+    })
+
+    const handleError = () => clearError({redirect: '/'})
+</script>
+
+<style scoped>
+</style>
