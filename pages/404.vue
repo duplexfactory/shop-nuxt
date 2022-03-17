375e1190ffa80bb348fb2072846e38f59fe794cd
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-03-17 17:57:46 +0800

    back home button

--- pages/404.vue
+++ pages/404.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="absolute flex flex-col justify-center" style="min-height: 100%; min-width: 100%">
       <div class="container mx-auto">
-        <div class="text-pink-500 text-8xl font-bold w-full text-center mb-4">
-          404
-        </div>
-        <div class="text-gray-600 text-xl w-full text-center">
-          頁面不存在或已被移除
+        <div class="w-full text-center ">
+          <div class="text-pink-500 text-8xl font-bold mb-4">404</div>
+          <div class="text-gray-600 text-xl">頁面不存在或已被移除</div>
+          <div class="mt-3 flex justify-center"><nuxt-link to="/" class="btn-primary">返回首頁</nuxt-link></div>
         </div>
       </div>
     </div>
