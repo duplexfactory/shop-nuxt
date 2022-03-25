0ba06b26fa550b7d438863bba49e3e0c8096a0c2
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-03-25 16:00:48 +0800

    not found component

--- error.vue
+++ error.vue
@@ -1,5 +1,15 @@
 <template>
   <NuxtLayout name="default">
-
+    <NotFound :error="error"/>
   </NuxtLayout>
 </template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+})
+</script>
+
+<style scoped>
+</style>
+
--- pages/404.vue
+++ pages/404.vue
@@ -1,21 +1,11 @@
 <template>
-    <div class="absolute flex flex-col justify-center" style="min-height: 100%; min-width: 100%">
-      <div class="container mx-auto">
-        <div class="w-full text-center ">
-          <div class="text-pink-500 text-8xl font-bold mb-4">404</div>
-          <div class="text-gray-600 text-xl">頁面不存在或已被移除</div>
-          <div class="mt-3 flex justify-center"><nuxt-link to="/" class="btn-primary">返回首頁</nuxt-link></div>
-        </div>
-      </div>
-    </div>
+    <NotFound :error="error"/>
 </template>
 
 <script setup>
     const props = defineProps({
         error: Object
     })
-
-    const handleError = () => clearError({redirect: '/'})
 </script>
 
 <style scoped>
