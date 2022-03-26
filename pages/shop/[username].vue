0ed2a5f289a2981e879f594e30015d9efab9fa05
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-03-26 13:10:44 +0800

    improve unverified page ui

--- pages/shop/[username].vue
+++ pages/shop/[username].vue
@@ -249,7 +249,7 @@ export default  {
                     <h2 class="px-5 py-2 md:px-6 md:py-3 cursor-pointer" :class="{'tab-selected': selectedIndex == 0}" @click="selectedIndex = 0">貼文</h2>
                     <h2 class="px-5 py-2 md:px-6 md:py-3 cursor-pointer" :class="{'tab-selected': selectedIndex == 1}" @click="selectedIndex = 1; fetchReviews();">評論</h2>
                 </div>
-                <div v-if="selectedIndex == 0" class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-4">
+                <div v-if="selectedIndex == 0" :class="verifiedPage ? 'grid-cols-2' : 'grid-cols-1'" class="grid md:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-4">
                   <!-- lazy component ratio for taking up space before scrolling to it -->
                   <lazy-component v-for="(media, i) in medias"
                                   :key="media.id + '-post-card'"
@@ -266,7 +266,7 @@ export default  {
                           @showMediaModal="showMediaModal(media.code)"
                           top-bar
                           :post-id="media.code"
-                          :fixed-aspect-ratio="3/5"
+                          :fixed-aspect-ratio="0"
                           :username="page.username"></MediaCardIGEmbed>
                     </template>
                   </lazy-component>
