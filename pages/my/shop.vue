cfaa56342353c9d59c4e73b78dd934b16668bdb0
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-03-30 00:50:24 +0800

    my shop wip

---
+++ pages/my/shop.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <div class="bg-white rounded-md border p-8">
+        <div class="text-xl md:text-2xl font-bold">
+          基本資料
+        </div>
+
+<!--        <div>名稱<input v-model="username" class="text-input-primary" type="text" name="username" placeholder="名稱"/></div>-->
+<!--        <div>全名<input v-model="username" class="text-input-primary" type="text" name="username" placeholder="全名"/></div>-->
+<!--        <div>描述<input v-model="username" class="text-input-primary" type="text" name="username" placeholder="描述"/></div>-->
+
+<!--        <div>分類<input v-model="username" class="text-input-primary" type="text" name="username" placeholder="分類"/></div>-->
+
+        <button @click="" class="mt-4 btn btn-primary">儲存</button>
+      </div>
+
+
+      <div class="bg-white rounded-md border p-8">
+        <div class="text-xl md:text-2xl font-bold">
+          詳細資料
+        </div>
+
+<!--        <input v-model="username" class="mt-4 block w-full text-input-primary" type="text" name="username" placeholder="用戶名">-->
+<!--        <input v-model="password" class="mt-4 block w-full text-input-primary" type="password" name="password" placeholder="密碼">-->
+<!--        <input v-model="confirmPassword" @keyup.enter="register" class="mt-4 block w-full text-input-primary" type="password" name="reenter-password" placeholder="重新輸入密碼">-->
+        <button @click="" class="mt-4 btn btn-primary">儲存</button>
+      </div>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+name: "shop"
+}
+</script>
+
+<style scoped>
+
+</style>
