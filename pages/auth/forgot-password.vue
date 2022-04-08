17ba448958e7b4359ef42aa104df400dcdc06ba1
zendagin <dicky1126277766238@gmail.com>
2022-04-08 16:23:30 +0900

    reset password

---
+++ pages/auth/forgot-password.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container mx-auto">
+        <div v-if="!done">
+            <h1 class="mt-8 text-xl md:text-2xl font-bold">
+                重設密碼
+            </h1>
+            <div class="text-gray-700">請輸入您的帳戶電郵以重設密碼</div>
+            <input v-model="email" class="mt-4 block text-input-primary" type="text" name="email"
+                   placeholder="電郵">
+            <button @click="confirm" class="mt-4 btn btn-primary" :disabled="disabled">確認</button>
+        </div>
+        <div v-else>
+            <h1 class="mt-8 text-xl text-gray-700">
+                已發送至{{ email }}，請檢查電郵以重設密碼
+            </h1>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import {getAuth, sendPasswordResetEmail} from "firebase/auth";
+    import {computed} from "@vue/reactivity";
+
+    const email = ref("")
+    const done = ref(false)
+
+    const emailRegEx = /\w+([\.-]?\w)*@\w+([\.-]?\w)*(\.\w{2,3})+/gm
+    const disabled = computed(() => !emailRegEx.test(email.value))
+
+    async function confirm() {
+        await sendPasswordResetEmail(getAuth(), email.value)
+        done.value = true
+    }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/auth/reset-password-done.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container mx-auto">
+        <h1 class="mt-8 text-xl text-gray-700">
+            已成功修改密碼，您現可使用新密碼登入
+        </h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
