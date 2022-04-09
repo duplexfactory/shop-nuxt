e040d115a433f8aac1dce1a95185605c1883fe5d
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-04-09 15:40:24 +0800

    basic info edit

--- pages/my/shop.vue
+++ pages/my/shop.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 
-import {getAuth, signOut} from "firebase/auth";
 import {useIsLoggedIn} from "~/composables/states";
 
 const router = useRouter();
@@ -23,6 +22,15 @@ watch(
 const {data} = await useFetch(`/api/shop?id=43808406274`);
 const {page: shop} = data.value
 
+const {categories, tagsLookup} = useTags()
+const selectedTag = ref("");
+function addSelectedTag() {
+  if (shop.tags.includes(selectedTag.value)) {
+    return;
+  }
+  shop.tags.push(selectedTag.value);
+}
+
 </script>
 
 <template>
@@ -33,12 +41,40 @@ const {page: shop} = data.value
           基本資料
         </div>
 
-        <div>名稱<input v-model="shop.username" class="ml-2 text-input-primary" type="text" name="username" placeholder="名稱"/></div>
-        <div>全名<input v-model="shop.fullName" class="ml-2 text-input-primary" type="text" name="fullName" placeholder="全名"/></div>
-<!--        <div>描述<input v-model="username" class="text-input-primary" type="text" name="username" placeholder="描述"/></div>-->
-
-<!--        <div>分類<input v-model="username" class="text-input-primary" type="text" name="username" placeholder="分類"/></div>-->
-
+        <div class="mt-2 flex items-center">
+          <span>名稱</span>
+          <input v-model="shop.username" class="ml-2 text-input-primary flex-1" type="text" name="username" placeholder="名稱"/>
+        </div>
+        <div class="mt-2 flex items-center">
+          <span>全名</span>
+          <input v-model="shop.fullName" class="ml-2 text-input-primary flex-1" type="text" name="fullName" placeholder="全名"/>
+        </div>
+        <div class="mt-2">
+          描述
+          <textarea v-model="shop.biography"
+                    class="mt-1 w-full border rounded-md p-2"
+                    rows="5"></textarea>
+        </div>
+        <div class="mt-2">
+          <div>分類</div>
+          <div class="flex items-center">
+            <select class="border rounded-sm" v-model="selectedTag">
+              <option value="" selected>選擇分類</option>
+              <optgroup v-for="category in categories" :key="category['id']" :label="category['label']">
+                <option v-for="tag in category.tags" :key="tag.id" :value="tag.id">{{ tag.label }}</option>
+              </optgroup>
+            </select>
+            <button class="btn-sm btn-outline ml-2" @click="addSelectedTag">+</button>
+          </div>
+          <div class="mt-1">
+            <div v-for="tag in shop.tags"
+                 :key="tag"
+                 class="tag border rounded-md p-2 mt-1 mr-2">
+              {{ `#${tagsLookup[tag]}` }}
+              <button @click="shop.tags = shop.tags.filter((t) => t !== tag)"><i class="spr-cancel"></i></button>
+            </div>
+          </div>
+        </div>
         <button @click="" class="mt-4 btn btn-primary">儲存</button>
       </div>
 
