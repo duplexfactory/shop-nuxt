5ce049b26f4c9ff9e0e050920a757a9b0516c14b
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-04-12 13:19:11 +0800

    my account unlink

--- pages/my/account.vue
+++ pages/my/account.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+
+const verifiedPage = false;
+const pageUsername = ref("shoperuse");
+
+</script>
+
 <template>
   <div>
     <div class="info-group">
@@ -12,6 +19,18 @@
           </div>
           <div class="table-cell pt-2">
             <input class="text-input-primary w-full" type="text" placeholder="用戶名"/>
+          </div>
+        </div>
+        <div class="table-row">
+          <div class="table-cell pr-2 pt-2">
+            Instagram帳戶
+          </div>
+          <div class="table-cell pt-2 text-right">
+            <button v-if="!verifiedPage" class="text-pink-600 py-2"><i class="spr-instagram"></i>立即連結</button>
+            <template v-else>
+              <a class="hover:underline text-pink-600 mr-2" :href="`https://www.instagram.com/${pageUsername}/`" target="_blank">{{ "@" + pageUsername }}</a>
+              <button class="text-gray-500 py-2">解除連結</button>
+            </template>
           </div>
         </div>
       </div>
@@ -52,12 +71,6 @@
     </div>
   </div>
 </template>
-
-<script steup lang="ts">
-
-const verifiedPage = true;
-
-</script>
 
 <style scoped>
 
