a04b0c8330c96d9bb962805770b13c9cb4f15661
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-04-12 13:04:22 +0800

    my revamp

---
+++ pages/my/account.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="info-group">
+      <div class="md:text-xl font-bold">
+        帳戶
+      </div>
+
+      <div class="table">
+        <div class="table-row">
+          <div class="table-cell pr-2 pt-2">
+            用戶名
+          </div>
+          <div class="table-cell pt-2">
+            <input class="text-input-primary w-full" type="text" placeholder="用戶名"/>
+          </div>
+        </div>
+      </div>
+      <button @click="" class="mt-4 btn btn-primary">儲存</button>
+    </div>
+    <div class="info-group">
+      <div class="md:text-xl font-bold">
+        變更密碼
+      </div>
+
+      <div class="table">
+        <div class="table-row">
+          <div class="table-cell pr-2 pt-2">
+            當前密碼
+          </div>
+          <div class="table-cell pt-2">
+            <input class="text-input-primary w-full" type="password" placeholder="當前密碼"/>
+          </div>
+        </div>
+        <div class="table-row">
+          <div class="table-cell pr-2 pt-2">
+            新密碼
+          </div>
+          <div class="table-cell pt-2">
+            <input class="text-input-primary w-full" type="password" placeholder="新密碼"/>
+          </div>
+        </div>
+        <div class="table-row">
+          <div class="table-cell pr-2 pt-2">
+            重新輸入新密碼
+          </div>
+          <div class="table-cell pt-2">
+            <input class="text-input-primary w-full" type="password" placeholder="重新輸入新密碼"/>
+          </div>
+        </div>
+      </div>
+      <button @click="" class="mt-4 btn btn-primary">儲存</button>
+    </div>
+  </div>
+</template>
+
+<script steup lang="ts">
+
+const verifiedPage = true;
+
+</script>
+
+<style scoped>
+
+.info-group {
+  @apply bg-white rounded-md border p-4 mb-4;
+}
+
+
+</style>
