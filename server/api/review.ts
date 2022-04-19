4daa558c99ea2b4ef80a8557e9a17502b8e2ff66
zendagin <dicky1126277766238@gmail.com>
2022-04-19 16:37:33 +0900

    review noCache

--- server/api/review.ts
+++ server/api/review.ts
@@ -1,8 +1,10 @@
 import {IncomingMessage, ServerResponse} from "http";
 import {reviewCollection} from "~/server/firebase/collections";
 import {assertMethod, useBody} from 'h3';
+import {noCache} from "~/server/util";
 
 export default async function (req: IncomingMessage, res: ServerResponse): Promise<{ id: string }> {
+    noCache(res)
     assertMethod(req, "POST")
 
     const {pagePk, mediaCode, content, rating} = await useBody<{ pagePk: number, mediaCode: string | undefined, content: string, rating: number}>(req);
@@ -13,7 +15,7 @@ export default async function (req: IncomingMessage, res: ServerResponse): Promi
         content,
         created: Date.now(),
         deleted: false,
-        ip: req.headers["cf-connecting-ip"] || req.connection.remoteAddress || req.socket.remoteAddress || ""
+        ip: req.headers["cf-connecting-ip"] as string || req.connection.remoteAddress || req.socket.remoteAddress || ""
     };
 
     const reference = await reviewCollection().add(review);
