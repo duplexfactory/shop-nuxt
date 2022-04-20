7c623051cfc4b8198c83274b0bbf203e40d53e33
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-04-20 15:00:48 +0800

    frontend update + prepare meta

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,22 +1,40 @@
 <script setup lang="ts">
   import Blog from "~/models/Blog"
   import dayjs from "dayjs";
+  import type {Ref} from "vue";
 
+  const config = useRuntimeConfig();
   const route = useRoute();
   const lastSeparatorIndex = (route.params.slug as string).lastIndexOf("-");
   const slug = (route.params.slug as string).slice(0, lastSeparatorIndex);
   const blogId = (route.params.slug as string).slice(lastSeparatorIndex + 1);
 
-  const blog: Blog = {
+  const blog: Ref<Blog & {id: string}> = ref<Blog & {id: string}>({
     id: "",
     created: 1650422141,
     title: "This is a title",
     slug: "",
-    metaTitle: "",
+    metaDesc: "",
     htmlContent: "",
+  })
+  const created = dayjs(blog.value.created * 1000).format('DD/MM/YYYY');
 
-  }
-  const created = dayjs(blog.created * 1000).format('DD/MM/YYYY');
+  // Meta
+  useMeta(computed(() => {
+    return {
+      title: `${blog.value.title} | IG Shop 推薦及評論平台 | Shoperuse`,
+      meta: [
+        {name: 'description', hid: 'description', content: blog.value.metaDesc},
+        {property: 'og:title', hid: 'og:title', content: `${blog.value.title} | IG Shop 推薦及評論平台 | Shoperuse`},
+        {property: 'og:url', hid: 'og:url', content: `${config.DOMAIN}/blog/${blog.value.slug}-${blog.value.id}`},
+        // {property: 'og:image', hid: 'og:image', content: `${page.value !== null ? page.value.profilePicUrl : ""}`},
+        // {property: 'og:image:height', hid: 'og:image:height', content: '150'},
+        // {property: 'og:image:width', hid: 'og:image:width', content: '150'},
+        {property: 'og:description', hid: 'og:description', content: blog.value.metaDesc}
+      ]
+    }
+  }))
+
 </script>
 
 <template>
