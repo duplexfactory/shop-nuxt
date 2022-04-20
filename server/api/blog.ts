b5edf9601f3d218ed4f357dfceee7f32fe4a324e
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-04-20 20:58:57 +0800

    get blog api

--- server/api/blog.ts
+++ server/api/blog.ts
@@ -1,37 +1,56 @@
 import {IncomingMessage, ServerResponse} from "http";
-import {noCache} from "~/server/util";
-import {assertMethod, useBody} from "h3";
+import {badRequest, noCache, notFound} from "~/server/util";
+import {assertMethod, isMethod, sendError, useBody, useQuery} from "h3";
 import {blogCollection} from "~/server/firebase/collections";
 import Blog from "~/models/Blog";
 
-export default async function (req: IncomingMessage, res: ServerResponse): Promise<{ id: string }> {
+export default async function (req: IncomingMessage, res: ServerResponse): Promise<{ id: string } | { blog: Blog }> {
     noCache(res)
-    assertMethod(req, "POST")
+    assertMethod(req, ["POST", "GET"])
 
-    const {
-        title,
-        slug,
-        metaDesc,
-        htmlContent,
-    } = await useBody<Omit<Blog, "id" | "created">>(req);
+    if (isMethod(req, "POST")) {
+        const {
+            title,
+            slug,
+            metaDesc,
+            htmlContent,
+        } = await useBody<Omit<Blog, "id" | "created">>(req);
 
-    // ID
-    const lastBlog = (await blogCollection().orderBy("id", "desc").pick("id").limit(1).get()).data();
-    let blogId = "1";
-    if (lastBlog.length !== 0) {
-        blogId = (Number(lastBlog.at(0).id) + 1).toString();
+        // ID
+        const lastBlog = (await blogCollection().orderBy("id", "desc").pick("id").limit(1).get()).data();
+        let blogId = "1";
+        if (lastBlog.length !== 0) {
+            blogId = (Number(lastBlog.at(0).id) + 1).toString();
+        }
+
+        await blogCollection().doc(blogId).set({
+            id: blogId,
+            created: Date.now(),
+            title,
+            slug,
+            metaDesc,
+            htmlContent,
+        })
+
+        return {
+            id: blogId
+        }
     }
+    else if (isMethod(req, "GET")) {
+        const {
+            id
+        } = useQuery(req) as { id: string };
 
-    await blogCollection().doc(blogId).set({
-        id: blogId,
-        created: Date.now(),
-        title,
-        slug,
-        metaDesc,
-        htmlContent,
-    })
+        if (!id) {
+            throw sendError(res, badRequest);
+        }
 
-    return {
-        id: blogId
+        const blogDoc = await blogCollection().doc(id).get();
+
+        if(!blogDoc.exists) {
+            sendError(res, notFound);
+        }
+
+        return {blog: blogDoc.data()};
     }
 }
