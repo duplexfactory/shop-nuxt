e9100dc0517f8d729763df5f8e35ce1a156cbef0
zendagin <dicky1126277766238@gmail.com>
2022-04-25 19:02:01 +0900

    api assert util

--- server/util.ts
+++ server/util.ts
@@ -1,5 +1,11 @@
-import {CompatibilityEvent} from "h3";
+import {CompatibilityEvent, createError, H3Error, sendError} from "h3";
 
 export function noCache(event: CompatibilityEvent) {
     event.res.setHeader("Cache-Control", "no-cache");
+}
+
+export function assert(event: CompatibilityEvent, condition: any, error: H3Error = badRequest): asserts condition {
+    if (!condition) {
+        throw sendError(event, error)
+    }
 }
