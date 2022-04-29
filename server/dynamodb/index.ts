00f7bc13da02561bc067b4b870929128bce6fa88
zendagin <dicky1126277766238@gmail.com>
2022-04-29 13:19:40 +0900

    dynamodb GetItemCommand

--- server/dynamodb/index.ts
+++ server/dynamodb/index.ts
@@ -1,25 +1,31 @@
-import {BatchWriteItemCommand, DynamoDBClient, QueryCommand, UpdateItemCommand} from "@aws-sdk/client-dynamodb";
-import {marshall, unmarshall} from "@aws-sdk/util-dynamodb";
-import IgMedia from "~/models/IgMedia";
-import dayjs from "dayjs";
+import {
+    BatchWriteItemCommand,
+    DynamoDBClient,
+    GetItemCommand,
+    QueryCommand,
+    UpdateItemCommand
+} from "@aws-sdk/client-dynamodb"
+import {marshall, unmarshall} from "@aws-sdk/util-dynamodb"
+import IgMedia from "~/models/IgMedia"
+import dayjs from "dayjs"
 // import {marshall, unmarshall} from "@aws-sdk/util-dynamodb";
 
-let init = false;
-let client: DynamoDBClient;
+let init = false
+let client: DynamoDBClient
 
 export async function initDynamo() {
     if (!init) {
-        const config = useRuntimeConfig();
+        const config = useRuntimeConfig()
         client = new DynamoDBClient({
             credentials: {
                 accessKeyId: config.DYNAMODB_ACCESS_KEY_ID,
                 secretAccessKey: config.DYNAMODB_SECRET_ACCESS_KEY
             },
             region: config.DYNAMODB_REGION
-        });
-        init = true;
+        })
+        init = true
     }
-    return client;
+    return client
 }
 
 export async function getPageMedias(pageId: string, limit?: number, before = dayjs().unix()) {
@@ -29,20 +35,17 @@ export async function getPageMedias(pageId: string, limit?: number, before = day
         ExpressionAttributeValues: marshall({":pageId": pageId, ":before": before}),
         ScanIndexForward: false,
         Limit: limit
-    }));
+    }))
 
-    return res.Items?.map(m => unmarshall(m)) as IgMedia[];
+    return res.Items?.map(m => unmarshall(m)) as IgMedia[]
 }
 
 export async function getMedia(pageId: number, takenAt: number): Promise<IgMedia | null> {
-    const res = await client.send(new QueryCommand({
+    const res = await client.send(new GetItemCommand({
         TableName: "media",
-        KeyConditionExpression: "pageId = :pageId AND takenAt = :takenAt",
-        ExpressionAttributeValues: marshall({":pageId": pageId, ":takenAt": takenAt}),
-        ScanIndexForward: false,
-        Limit: 1
-    }));
-    return res.Items.map(i => unmarshall(i))[0] as IgMedia;
+        Key: marshall({pageId, takenAt})
+    }))
+    return res.Item ? unmarshall(res.Item) as IgMedia : null
 }
 
 async function queryMediaByCode(code: string) {
@@ -53,28 +56,27 @@ async function queryMediaByCode(code: string) {
         ExpressionAttributeValues: marshall({":code": code}),
         ScanIndexForward: false,
         Limit: 1
-    }));
+    }))
 }
 
 export async function getMediaByCode(code: string): Promise<IgMedia | null> {
-    const res = await queryMediaByCode(code);
+    const res = await queryMediaByCode(code)
 
     if (res?.Count) {
-        const {pageId, takenAt} = unmarshall(res.Items[0]);
-        return getMedia(pageId, takenAt);
-    } else return undefined;
+        const {pageId, takenAt} = unmarshall(res.Items[0])
+        return getMedia(pageId, takenAt)
+    } else return undefined
 }
 
 export async function patchMediaByCode(code: string, patchItem): Promise<boolean> {
-    const res = await queryMediaByCode(code);
+    const res = await queryMediaByCode(code)
     if (res?.Count) {
-        const {pagePk, takenAt} = unmarshall(res.Items[0]);
+        const {pagePk, takenAt} = unmarshall(res.Items[0])
         await update("medias", {
             pagePk, takenAt
         }, patchItem)
         return true
-    }
-    else {
+    } else {
         return false
     }
 }
@@ -86,7 +88,7 @@ export async function patchMediaByCode(code: string, patchItem): Promise<boolean
  * @param {object} item // Object containing updates for target item
  */
 const update = async (tableName, key, item) => {
-    const itemKeys = Object.keys(item);
+    const itemKeys = Object.keys(item)
 
     // When we do updates we need to tell DynamoDB what fields we want updated.
     // If that's not annoying enough, we also need to be careful as some field names
@@ -95,14 +97,20 @@ const update = async (tableName, key, item) => {
     // field mapping in ExpressionAttributeNames. The same has to be done with the actual
     // value as well. They are prefixed with ":value" and mapped in ExpressionAttributeValues
     // along with their actual value
-    const { Attributes } = await client.send(new UpdateItemCommand({
+    const {Attributes} = await client.send(new UpdateItemCommand({
         TableName: tableName,
         Key: marshall(key),
-        ReturnValues: 'ALL_NEW',
-        UpdateExpression: `SET ${itemKeys.map((k, index) => `#field${index} = :value${index}`).join(', ')}`,
-        ExpressionAttributeNames: itemKeys.reduce((accumulator, k, index) => ({ ...accumulator, [`#field${index}`]: k }), {}),
-        ExpressionAttributeValues: marshall(itemKeys.reduce((accumulator, k, index) => ({ ...accumulator, [`:value${index}`]: item[k] }), {})),
-    }));
+        ReturnValues: "ALL_NEW",
+        UpdateExpression: `SET ${itemKeys.map((k, index) => `#field${index} = :value${index}`).join(", ")}`,
+        ExpressionAttributeNames: itemKeys.reduce((accumulator, k, index) => ({
+            ...accumulator,
+            [`#field${index}`]: k
+        }), {}),
+        ExpressionAttributeValues: marshall(itemKeys.reduce((accumulator, k, index) => ({
+            ...accumulator,
+            [`:value${index}`]: item[k]
+        }), {})),
+    }))
 
-    return unmarshall(Attributes);
-};
+    return unmarshall(Attributes)
+}
