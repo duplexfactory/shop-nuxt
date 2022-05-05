1e58d103b48388ebe35d6085d674222c33a68a72
zendagin <dicky1126277766238@gmail.com>
2022-05-05 15:55:54 +0900

    show login

--- components/TopHeader.vue
+++ components/TopHeader.vue
@@ -18,29 +18,35 @@
         <div class="hidden sm:block flex-1">
           <div class="flex w-full">
             <div class="dropdown flex-1" style="max-width: 350px;">
-              <input @keyup.enter="search" @focusin="searchInputFocusIn" @focusout="searchInputFocusOut" v-model="searchText" class="search-input" placeholder="搜尋 商店 或 貼文" type="search" autocomplete="off" id="search" name="search" />
+              <input @keyup.enter="search" @focusin="searchInputFocusIn" @focusout="searchInputFocusOut"
+                     v-model="searchText" class="search-input" placeholder="搜尋 商店 或 貼文" type="search" autocomplete="off"
+                     id="search" name="search"/>
               <div v-if="showSearchDropdown && searchText !== ''" class="search-menu">
                 <template v-if="tagsSearchResult.length !== 0">
                   <div class="px-4 py-2 text-sm bg-gray-50">分類</div>
                   <div class="px-4 pb-2">
                     <div class="line-clamp-2" style="font-size: 0px;">
                       <!-- Use mousedown to take precedence over focusout -->
-                      <button @mousedown="tagPressed(tag.id)" v-for="tag in tagsSearchResult" :key="tag.id" class="tag mt-2 mr-4">{{ `#${tag.label}` }}</button>
+                      <button @mousedown="tagPressed(tag.id)" v-for="tag in tagsSearchResult" :key="tag.id"
+                              class="tag mt-2 mr-4">{{ `#${tag.label}` }}
+                      </button>
                     </div>
                   </div>
                 </template>
                 <template v-if="searchResults.length !== 0">
                   <div class="px-4 py-2 text-sm bg-gray-50">搜尋結果</div>
-                  <button @mousedown="quickSearchResultPressed(result.username)" class="px-4 py-2 text-sm block" v-for="result in searchResults.slice(0, 10)">{{ result.username }}</button>
+                  <button @mousedown="quickSearchResultPressed(result.username)" class="px-4 py-2 text-sm block"
+                          v-for="result in searchResults.slice(0, 10)">{{ result.username }}
+                  </button>
                 </template>
               </div>
             </div>
             <button @click="search" class="border border-pink-400 btn-primary !rounded-none">搜尋</button>
           </div>
-<!--          <ul v-if="showSearchDropdown" class="search-menu">-->
-<!--            <li v-for="category in categoriesSearchResult" :key="category.id" class="px-4 py-1">{{ category.label }}</li>-->
-<!--            <li v-for="tag in tagsSearchResult" :key="tag.id" class="px-4 py-1">{{ tag.label }}</li>-->
-<!--          </ul>-->
+          <!--          <ul v-if="showSearchDropdown" class="search-menu">-->
+          <!--            <li v-for="category in categoriesSearchResult" :key="category.id" class="px-4 py-1">{{ category.label }}</li>-->
+          <!--            <li v-for="tag in tagsSearchResult" :key="tag.id" class="px-4 py-1">{{ tag.label }}</li>-->
+          <!--          </ul>-->
 
         </div>
 
@@ -48,15 +54,13 @@
 
       <!-- right -->
       <div class="hidden md:block">
-          <template v-if="false">
-            <nuxt-link v-if="!isLoggedIn" to="/verify" class="btn btn-sm btn-primary mr-4">認證我的商店</nuxt-link>
-            <nuxt-link v-if="!isLoggedIn" to="/login/shop" class="btn btn-sm btn-outline mr-4">商戶登入</nuxt-link>
+        <nuxt-link v-if="!isLoggedIn" to="/verify" class="btn btn-sm btn-primary mr-4">認證我的商店</nuxt-link>
+        <nuxt-link v-if="!isLoggedIn" to="/login/shop" class="btn btn-sm btn-outline mr-4">商戶登入</nuxt-link>
 
-            <nuxt-link v-if="isLoggedIn" to="/my/shop" class="btn btn-sm btn-primary mr-4">我的商店</nuxt-link>
-            <button v-if="isLoggedIn" @click="logout" class="btn btn-sm btn-outline mr-4">登出</button>
-          </template>
+        <nuxt-link v-if="isLoggedIn" to="/my/shop" class="btn btn-sm btn-primary mr-4">我的商店</nuxt-link>
+        <button v-if="isLoggedIn" @click="logout" class="btn btn-sm btn-outline mr-4">登出</button>
 
-        <AgeRestrictionToggle />
+        <AgeRestrictionToggle/>
       </div>
 
     </div>
@@ -67,125 +71,125 @@
 
 <script setup lang="ts">
 
-import useSearch from "~/composables/useSearch";
-import {useShowSearchModal} from "~/composables/states";
-import {getAuth} from "firebase/auth";
+  import useSearch from "~/composables/useSearch"
+  import {useShowSearchModal} from "~/composables/states"
+  import {getAuth} from "firebase/auth"
 
-const {ageRestrictedCategories} = useTags();
-const {
-  searchResults,
-  searchResultTotalCount,
-  search: quickSearch
-} = useSearch();
+  const {ageRestrictedCategories} = useTags()
+  const {
+    searchResults,
+    searchResultTotalCount,
+    search: quickSearch
+  } = useSearch()
 
-const showSearchModal = useShowSearchModal();
+  const showSearchModal = useShowSearchModal()
 
-// Login
-const isLoggedIn = useIsLoggedIn();
-async function logout() {
-  const auth = getAuth();
-  await auth.signOut();
-  isLoggedIn.value = false;
-}
+  // Login
+  const isLoggedIn = useIsLoggedIn()
+
+  async function logout() {
+    const auth = getAuth()
+    await auth.signOut()
+    isLoggedIn.value = false
+  }
 
 </script>
 
 <script lang="ts">
 
-import {PageSearchQuery} from "~/models/PageSearchQuery";
-import {PaginationQuery} from "~/models/PaginationQuery";
+  import {PageSearchQuery} from "~/models/PageSearchQuery"
+  import {PaginationQuery} from "~/models/PaginationQuery"
 
-export default  {
-  data(): {
-    showSearchDropdown: boolean,
-    searchText: string,
-    categoriesSearchResult: {id: string, label: string, tags: {id: string, label: string}[]}[],
-    tagsSearchResult: {id: string, label: string}[],
-  } {
-    return {
-      showSearchDropdown: false,
-      searchText: '',
-      categoriesSearchResult: [],
-      tagsSearchResult: [],
-    }
-  },
-  methods: {
-    searchInputFocusIn: function () {
-      this.showSearchDropdown = true;
-    },
-    searchInputFocusOut: function () {
-      this.showSearchDropdown = false;
-    },
-    tagPressed: function(tagId: string) {
-      let query = this.$route.path == "/search" ? {...this.$route.query} : {};
-      Object.assign(query, { tag: tagId });
-      this.$router.push({path: '/search', query});
-    },
-    quickSearchResultPressed: function(username: string) {
-      this.$router.push({path: `/shop/${username}`});
-    },
-    search: function () {
-      let query = this.$route.path == "/search" ? {...this.$route.query} : {};
-      if (this.searchText != "") {
-        query["keyword"] = this.searchText;
+  export default {
+    data(): {
+      showSearchDropdown: boolean,
+      searchText: string,
+      categoriesSearchResult: { id: string, label: string, tags: { id: string, label: string }[] }[],
+      tagsSearchResult: { id: string, label: string }[],
+    } {
+      return {
+        showSearchDropdown: false,
+        searchText: "",
+        categoriesSearchResult: [],
+        tagsSearchResult: [],
       }
-      else {
-        delete query["keyword"];
-      }
-      delete query["page"];
-
-      this.$router.push({
-        path: "/search",
-        query
-      });
-    }
-  },
-  watch: {
-    searchText: async function (searchText) {
-      this.categoriesSearchResult = [];
-      this.tagsSearchResult = [];
-      if (searchText === '') {
-        return;
-      }
-
-      for (const c of this.ageRestrictedCategories as {id: string, label: string, tags: {id: string, label: string}[]}[]) {
-        if (c.id.includes(searchText) || c.label.includes(searchText)) {
-          this.categoriesSearchResult.push(c);
+    },
+    methods: {
+      searchInputFocusIn: function () {
+        this.showSearchDropdown = true
+      },
+      searchInputFocusOut: function () {
+        this.showSearchDropdown = false
+      },
+      tagPressed: function (tagId: string) {
+        let query = this.$route.path == "/search" ? {...this.$route.query} : {}
+        Object.assign(query, {tag: tagId})
+        this.$router.push({path: "/search", query})
+      },
+      quickSearchResultPressed: function (username: string) {
+        this.$router.push({path: `/shop/${username}`})
+      },
+      search: function () {
+        let query = this.$route.path == "/search" ? {...this.$route.query} : {}
+        if (this.searchText != "") {
+          query["keyword"] = this.searchText
+        } else {
+          delete query["keyword"]
         }
-        this.tagsSearchResult.push(...c.tags.filter((t) => c.id.includes(searchText) || c.label.includes(searchText) || t.id.includes(searchText) || t.label.includes(searchText)));
-      }
+        delete query["page"]
 
-      await this.quickSearch(new PageSearchQuery(
-          searchText,
-      ), new PaginationQuery(0, 11));
+        this.$router.push({
+          path: "/search",
+          query
+        })
+      }
     },
+    watch: {
+      searchText: async function (searchText) {
+        this.categoriesSearchResult = []
+        this.tagsSearchResult = []
+        if (searchText === "") {
+          return
+        }
+
+        for (const c of this.ageRestrictedCategories as { id: string, label: string, tags: { id: string, label: string }[] }[]) {
+          if (c.id.includes(searchText) || c.label.includes(searchText)) {
+            this.categoriesSearchResult.push(c)
+          }
+          this.tagsSearchResult.push(...c.tags.filter((t) => c.id.includes(searchText) || c.label.includes(searchText) || t.id.includes(searchText) || t.label.includes(searchText)))
+        }
+
+        await this.quickSearch(new PageSearchQuery(
+          searchText,
+        ), new PaginationQuery(0, 11))
+      },
+    }
   }
-}
 
 </script>
 
 <style scoped>
 
-.search-input {
-  @apply border py-2 px-4 text-md w-full;
-}
+  .search-input {
+    @apply border py-2 px-4 text-md w-full;
+  }
 
-.dropdown {
-  @apply relative;
-}
+  .dropdown {
+    @apply relative;
+  }
 
-.dropdown .search-menu {
-  z-index: 1000;
-  @apply absolute bg-white w-full shadow-lg;
-}
+  .dropdown .search-menu {
+    z-index: 1000;
+    @apply absolute bg-white w-full shadow-lg;
+  }
 
-.dropdown .search-menu li {
-  @apply px-8 py-2 text-center;
-}
+  .dropdown .search-menu li {
+    @apply px-8 py-2 text-center;
+  }
 
-[class^="spr-"]:before, [class*=" spr-"]:before {
-  margin: 0px !important;
-}
+  [class^="spr-"]:before, [class*=" spr-"]:before {
+    margin: 0px !important;
+  }
 
 
 </style>
