d4ede051693136cf83930d64c854cfa3d13a8800
Victor Kwok <victorkwok97@gmail.com>
2022-05-11 13:21:37 +0800

    fix: footer add verify my shop

--- components/BottomFooter.vue
+++ components/BottomFooter.vue
@@ -2,8 +2,8 @@
     <div class="bg-gray-100 text-sm text-gray-600">
         <div class="container mx-auto py-4">
 
-            <div class="flex items-center">
-<!--                <nuxt-link to="/verify" class="mr-6">認證我的商店</nuxt-link>-->
+            <div class="">
+                <nuxt-link to="/verify" class="mr-6">認證我的商店</nuxt-link>
                 <nuxt-link to="/suggest-shop" class="mr-6">提供商店資料</nuxt-link>
                 <nuxt-link to="/takedown" class="mr-6">移除商店資料</nuxt-link>
 <!--                <div class="mr-6">關於我們</div>-->
