8f17715913616d300a4f6ff3270e65f433138691
zendagin <dicky1126277766238@gmail.com>
2022-05-11 13:30:22 +0900

    fix get id token

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,11 +1,18 @@
-import {getAuth, getIdToken} from "firebase/auth"
+import {getAuth, getIdToken, onAuthStateChanged, User} from "firebase/auth"
 import {useCurrentUser} from "~/composables/states"
 
 export default function useAuth() {
-    const _getIdToken = (forceRefresh?: boolean) => getIdToken(useCurrentUser().value, forceRefresh)
+    const auth = getAuth()
+    const _getIdToken = async (forceRefresh?: boolean) => {
+        if (useCurrentUser().value) return getIdToken(useCurrentUser().value, forceRefresh)
+
+        return new Promise((resolve) =>
+            onAuthStateChanged(auth, async (user?: User) => resolve(await getIdToken(user, forceRefresh)))
+        )
+    }
     const getAuthHeader = async (forceRefresh?: boolean) => new Headers({authorization: `Bearer ${await _getIdToken(forceRefresh)}`})
     return {
-        auth: getAuth(),
+        auth,
         getIdToken: _getIdToken,
         getAuthHeader
     }
