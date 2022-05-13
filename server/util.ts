4c9eaee83d7739d8f5f4ce4852a63a5fc6436678
zendagin <dicky1126277766238@gmail.com>
2022-05-13 19:14:25 +0900

    assertion failure

--- server/util.ts
+++ server/util.ts
@@ -3,14 +3,17 @@ import {badRequest} from "~/utils/h3Error"
 import type {DecodedIdToken} from "firebase-admin/auth"
 
 export function noCache(event: CompatibilityEvent) {
-    event.res.setHeader("Cache-Control", "no-cache");
+    event.res.setHeader("Cache-Control", "no-cache")
 }
 
 export function assert(event: CompatibilityEvent, condition: any, error: H3Error = badRequest): asserts condition {
-    if (!condition) throw sendError(event, error)
+    if (!condition) {
+        sendError(event, error)
+        throw new Error("assertion failure")
+    }
 }
 
 export function getAuth(event: CompatibilityEvent, block = true) {
-    if(!event.context.auth && block) sendError(event, createError({statusCode: 401, statusMessage: "Not Authorized"}))
+    if (!event.context.auth && block) sendError(event, createError({statusCode: 401, statusMessage: "Not Authorized"}))
     return event.context.auth as DecodedIdToken
 }
