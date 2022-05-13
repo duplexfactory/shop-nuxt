bd58bf1340cdfbb6c4936414f17fc44ab4cf8750
zendagin <dicky1126277766238@gmail.com>
2022-05-13 18:03:23 +0900

    temp disable cache

--- server/middleware/cache.ts
+++ server/middleware/cache.ts
@@ -1,6 +1,6 @@
 import type { IncomingMessage, ServerResponse } from 'http'
 
 export default defineEventHandler((event) => {
-    if(!event.res.getHeader('Cache-Control'))
-        event.res.setHeader('Cache-Control', 'max-age=28800');
+    // if(!event.res.getHeader('Cache-Control'))
+    //     event.res.setHeader('Cache-Control', 'max-age=28800');
 })
