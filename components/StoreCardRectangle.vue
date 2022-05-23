0aebdf8fc76121b959fea8e482b771bacc305652
Victor Kwok <victorkwok97@gmail.com>
2022-05-23 12:44:04 +0800

    fix: StoreCardRectangle no fans count

--- components/StoreCardRectangle.vue
+++ components/StoreCardRectangle.vue
@@ -17,9 +17,9 @@
         <div class="hidden sm:block text-gray-400 font-light text-xs">最後活躍 {{ lastActive }}</div>
 
         <div class="mt-2 2xl:mt-4 hidden sm:flex text-sm text-gray-500">
-          <div v-if="shop.followerCount" class="text-center flex-1">
+          <div class="text-center flex-1">
             <div>粉絲</div>
-            <div>{{ shop.followerCount.toLocaleString() }}</div>
+            <div>{{ !!shop.followerCount ? shop.followerCount.toLocaleString() : "-" }}</div>
           </div>
           <div class="bg-gray-300 mx-2" style="width: 1px;"></div>
           <div class="text-center flex-1">
