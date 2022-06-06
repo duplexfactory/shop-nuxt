d90594138c3f7e6d787a900f80be81d4f15cf386
Victor Kwok <victorkwok97@gmail.com>
2022-06-06 10:31:09 +0800

    fix: signInWithRedirect

--- composables/useFbLogin.ts
+++ composables/useFbLogin.ts
@@ -1,4 +1,4 @@
-import {FacebookAuthProvider, getAuth, signInWithPopup} from "firebase/auth";
+import {FacebookAuthProvider, getAuth, signInWithPopup, signInWithRedirect} from "firebase/auth";
 
 export default function useFbLogin() {
 
@@ -7,28 +7,33 @@ export default function useFbLogin() {
 
         const auth = getAuth();
         // auth.languageCode = ;
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                // The signed-in user info.
-                const user = result.user;
 
-                // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-                const credential = FacebookAuthProvider.credentialFromResult(result);
-                const accessToken = credential.accessToken;
+        signInWithRedirect(auth, provider).then((result) => {
 
-                // ...
-            })
-            .catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.email;
-                // The AuthCredential type that was used.
-                const credential = FacebookAuthProvider.credentialFromError(error);
+        });
 
-                // ...
-            });
+        // signInWithPopup(auth, provider)
+        //     .then((result) => {
+        //         // The signed-in user info.
+        //         const user = result.user;
+        //
+        //         // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        //         const credential = FacebookAuthProvider.credentialFromResult(result);
+        //         const accessToken = credential.accessToken;
+        //
+        //         // ...
+        //     })
+        //     .catch((error) => {
+        //         // Handle Errors here.
+        //         const errorCode = error.code;
+        //         const errorMessage = error.message;
+        //         // The email of the user's account used.
+        //         const email = error.email;
+        //         // The AuthCredential type that was used.
+        //         const credential = FacebookAuthProvider.credentialFromError(error);
+        //
+        //         // ...
+        //     });
     }
 
     return {
