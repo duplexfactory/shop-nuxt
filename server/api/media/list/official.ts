8932867dac2b96ed944ff1e1a19e42b616fe2988
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-06-09 01:21:57 +0800

    fix: media list official

--- server/api/media/list/official.ts
+++ server/api/media/list/official.ts
@@ -19,6 +19,15 @@ export default defineEventHandler(async (event) => {
     const p = await igAuthCollection.findOne({pageId: id}, {projection: {accessToken: 1}})
     assert(p, notFound)
 
+    // Get new medias from official api.
+
+    const medias: IgMedia[] = await fetchIgMedias(id, p.accessToken, true, {
+        limit: Number(limit),
+        until: until ? Number(until) - 1 : undefined,
+    })
+
+    // const since: number | undefined = medias.length ? medias[0].takenAt : undefined
+
     // Get existing medias from dynamo for price and patchPrice.
     initDynamo()
     const existingMedias = await getPageMedias(
@@ -27,50 +36,49 @@ export default defineEventHandler(async (event) => {
         until ? Number(until) - 1 : undefined,
     )
 
-    let medias: IgMedia[] = []
-
-    if (!until) {
-        // Get new medias from official api.
-        const since: number | undefined = existingMedias.length ? existingMedias[0].takenAt : undefined
-        medias = await fetchIgMedias(id, p.accessToken, true, {
-            limit: Number(limit),
-            until: until ? Number(until) - 1 : undefined,
-            since
-        })
-        if (medias.length !== 0) {
-            // Contains not crawled data. Process and save to db.
-            medias.forEach((m) => {
-                if (!!m.caption) {
-                    const price = detectPrice(m.caption)
-                    if (price !== undefined)
-                        m.price = price
-                }
-            })
-
-            const rmUrl = medias.map(m => {
-                const {mediaUrl, ...props} = m
-                return props
-            })
-            await saveMedias(rmUrl)
-
-            const {media_count} = await fetchIgProfile(p.accessToken)
-
-            // Update media info of page.
-            const lastMedia = rmUrl[0]
-            const update = {
-                lastMedia: lastMedia.takenAt,
-                lastActivity: lastMedia.takenAt,
-                lastMediaData: lastMedia,
-                mediaCount: media_count,
-                mediaCodes: medias.slice(0, 3).map((m) => m.code)
-            }
-            await pageSearchCollection.updateOne({_id: id}, {$set: update})
-            await pageCollection().doc(id).update(update)
+    const newMedias: IgMedia[] = []
+    medias.forEach((m) => {
+        const existingMedia = existingMedias.find((em) => em.code === m.code)
+        if (!!existingMedia) {
+            m.price = existingMedia.price
         }
+        else {
+            newMedias.push(m)
+        }
+    })
+    if (newMedias.length !== 0) {
+        // Contains not crawled data. Process and save to db.
+        newMedias.forEach((m) => {
+            if (!!m.caption) {
+                const price = detectPrice(m.caption)
+                if (price !== undefined)
+                    m.price = price
+            }
+        })
+        const rmUrl = newMedias.map(m => {
+            const {mediaUrl, ...props} = m
+            return props
+        })
+        await saveMedias(rmUrl)
+
+        const {media_count} = await fetchIgProfile(p.accessToken)
+
+        // Update media info of page.
+        const lastMedia = rmUrl[0]
+        const update = {
+            lastMedia: lastMedia.takenAt,
+            lastActivity: lastMedia.takenAt,
+            lastMediaData: lastMedia,
+            mediaCount: media_count,
+            mediaCodes: medias.slice(0, 3).map((m) => m.code)
+        }
+        await pageSearchCollection.updateOne({_id: id}, {$set: update})
+        await pageCollection().doc(id).update(update)
     }
 
+
     return {
-        medias: medias.concat(...existingMedias)
+        medias: medias
     } as unknown as JSONValue
     // { medias: IgMedia[] }
 })
