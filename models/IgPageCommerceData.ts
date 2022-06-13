dc445ffba681bcc6709d3dc8bb9a7edabcfb22f0
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-06-13 21:37:52 +0800

    feat: PaymentMethodData

--- models/IgPageCommerceData.ts
+++ models/IgPageCommerceData.ts
@@ -5,4 +5,37 @@ export interface IgPageCommerceData {
     _id: string; // Same as IgPage _id.
     discount?: Discount;
     mailing: Mailing[];
+    paymentMethodData: PaymentMethodData[];
 }
+
+enum PaymentType {
+    BANK_TRANSFER,
+    FPS,
+    PAYME,
+    WECHAT_PAY,
+    ALIPAY
+}
+
+export interface PaymentMethodData {
+    method: PaymentType;
+}
+
+export interface BankTransferPaymentMethodData extends PaymentMethodData {
+    method: PaymentType.BANK_TRANSFER;
+    bank: string;
+    accountNumber: number;
+    accountName: string;
+}
+
+export interface FPSPaymentMethodData extends PaymentMethodData {
+    method: PaymentType.FPS;
+    phone: string;
+    account: string; // 預設帳戶，如需選擇帳戶，請選擇 HSBC 匯豐銀行
+    accountName: string;
+}
+
+export interface QRCodePaymentMethodData extends PaymentMethodData {
+    method: PaymentType.PAYME | PaymentType.WECHAT_PAY | PaymentType.ALIPAY;
+    qrCodeUrl: string;
+}
+
