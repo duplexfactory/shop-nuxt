a4207c43efad31c3bd86caf0d80de4af1a359595
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-07-21 19:27:30 +0800

    feat: get shop data

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container mx-auto">
-    <div v-for="shop in shops" :key="shop">
-      {{ mediasGrouped[shop] }}
+    <div v-for="pageId in pageIds" :key="pageId" class="p-4">
+      {{ pages[pageId].username }}
+      <div v-for="media in mediasGrouped[pageId]" :key="media.code">
+        {{ media }}
+      </div>
+<!--      {{ mediasGrouped[shop] }}-->
     </div>
   </div>
 </template>
@@ -13,9 +17,10 @@ import type {Ref} from "vue"
 import Dict = NodeJS.Dict
 import {IgMediaCommerceData} from "~/models/IgMediaCommerceData";
 import {IgPageCommerceData} from "~/models/IgPageCommerceData";
+import IgPage from "~/models/IgPage";
 
 const cart: Ref<{code: string, quantity: number}[]> = ref([])
-const shops = computed(() => Array( ...new Set(mediaList.value.map((m) => m.pageId))))
+const pageIds = computed(() => Array( ...new Set(mediaList.value.map((m) => m.pageId))))
 
 const mediaList: Ref<IgMedia[]> = ref([])
 const mediasGrouped = computed(() => {
@@ -57,9 +62,24 @@ async function fetchMediaCommerceData() {
   }
 }
 
+const pages: Ref<Dict<IgPage>> = ref({})
+async function fetchPages() {
+  const {
+    data,
+    error
+  } = await useFetch('/api/shop', {
+    params: {
+      ids: pageIds.value.join(",")
+    }
+  })
+  if (!!data.value) {
+    pages.value = data.value["pages"]
+  }
+}
+
 const pageCommerceData: Ref<Dict<IgPageCommerceData>> = ref({})
 async function fetchPageCommerceData() {
-  await Promise.all(shops.value.map(async (s) => {
+  await Promise.all(pageIds.value.map(async (s) => {
     const {
       data,
       error
@@ -79,6 +99,7 @@ onMounted(async () => {
     await nextTick(async () => {
       await fetchMedias()
       await fetchMediaCommerceData()
+      await fetchPages()
       await fetchPageCommerceData()
     })
   }
