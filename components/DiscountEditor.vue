83de30eecb95616ecedc4963d2c0e3faaef6a44f
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-08-25 10:59:45 +0800

    discount editor responsiveness

--- components/DiscountEditor.vue
+++ components/DiscountEditor.vue
@@ -6,38 +6,47 @@
            placeholder="聖誕大減價"/>
 
     <div class="mt-4">
-      <div class="mb-1">折扣條件</div>
-      <div class="flex items-center">
-        <lazy-spr-select class="mr-2" v-model="value.thresholdType">
+      <div>折扣條件</div>
+      <div class="flex items-center flex-wrap">
+
+        <lazy-spr-select class="mr-2 flex-shrink-0 mt-1" v-model="value.thresholdType">
           <option :value="ThresholdType.COUNT">數量</option>
           <option :value="ThresholdType.VALUE">價錢</option>
         </lazy-spr-select>
-        <span>滿{{value.thresholdType === ThresholdType.VALUE ? " HK$" : ""}}</span>
-        <input v-model.number="value.threshold"
-               type="number"
-               class="text-input-primary mx-2"
-               :placeholder="value.thresholdType === ThresholdType.VALUE ? '金額' : '數量'"/>
-        <span v-if="value.thresholdType === ThresholdType.COUNT">件</span>
+
+        <div class="flex items-center flex-1 mt-1">
+          <span>滿{{value.thresholdType === ThresholdType.VALUE ? " HK$" : ""}}</span>
+          <input v-model.number="value.threshold"
+                 type="number"
+                 class="text-input-primary mx-2 flex-1"
+                 style="min-width: 100px; max-width: 200px; width: 0;"
+                 :placeholder="value.thresholdType === ThresholdType.VALUE ? '金額' : '數量'"/>
+          <span v-if="value.thresholdType === ThresholdType.COUNT">件</span>
+        </div>
+
       </div>
     </div>
 
     <div class="mt-4">
-      <div class="mb-1">折扣類型</div>
-      <div class="flex items-center">
-        <lazy-spr-select class="mr-2" v-model="value.discountType">
+      <div>折扣類型</div>
+      <div class="flex items-center flex-wrap">
+        <lazy-spr-select class="mr-2 flex-shrink-0 mt-1" v-model="value.discountType">
           <option :value="DiscountType.FLAT">實數</option>
           <option :value="DiscountType.RATIO">百分比</option>
         </lazy-spr-select>
-        <div class="flex">
-          <span v-if="value.discountType === DiscountType.FLAT" class="text-input-prefix-primary">- HK$</span>
+
+        <div class="flex items-center flex-1 mt-1">
+          <span v-if="value.discountType === DiscountType.FLAT" class="text-input-prefix-primary flex-shrink-0">- HK$</span>
           <input size="1"
                  v-model.number="value.discount"
                  type="number"
-                 class="text-input-primary"
+                 class="text-input-primary flex-1"
                  :class="{'text-input-primary--prefixed': value.discountType === DiscountType.FLAT, 'text-input-primary--suffixed': value.discountType === DiscountType.RATIO}"
+                 style="min-width: 100px; max-width: 200px; width: 0;"
                  placeholder="折扣"/>
-          <span v-if="value.discountType === DiscountType.RATIO" class="text-input-suffix-primary">% off</span>
+          <span v-if="value.discountType === DiscountType.RATIO" class="text-input-suffix-primary flex-shrink-0">% off</span>
         </div>
+
       </div>
 
     </div>
--- components/MailingDiscountEditor.vue
+++ components/MailingDiscountEditor.vue
@@ -5,18 +5,22 @@
            placeholder="優惠名稱（選填）"/>
     <div class="mt-4 text-left">
       <div class="mb-1">優惠條件</div>
-      <div class="flex items-center">
-        <lazy-spr-select class="mr-2" v-model="value.thresholdType">
+      <div class="flex items-center flex-wrap">
+        <lazy-spr-select class="mr-2 flex-shrink-0 mt-1" v-model="value.thresholdType">
           <option :value="ThresholdType.COUNT">數量</option>
           <option :value="ThresholdType.VALUE">價錢</option>
         </lazy-spr-select>
-        <span>滿{{value.thresholdType === ThresholdType.VALUE ? " HK$" : ""}}</span>
-        <input v-model.number="value.threshold"
-               type="number"
-               class="text-input-primary mx-2"
-               :placeholder="value.thresholdType === ThresholdType.VALUE ? '金額' : '數量'"/>
-        <span v-if="value.thresholdType === ThresholdType.COUNT">件</span>
-        <span>免郵</span>
+
+        <div class="flex items-center flex-1 mt-1">
+          <span>滿{{value.thresholdType === ThresholdType.VALUE ? " HK$" : ""}}</span>
+          <input v-model.number="value.threshold"
+                 type="number"
+                 class="text-input-primary mx-2 flex-1"
+                 style="min-width: 100px; max-width: 200px; width: 0;"
+                 :placeholder="value.thresholdType === ThresholdType.VALUE ? '金額' : '數量'"/>
+          <span v-if="value.thresholdType === ThresholdType.COUNT">件</span>
+          <span>免郵</span>
+        </div>
       </div>
     </div>
   </div>
