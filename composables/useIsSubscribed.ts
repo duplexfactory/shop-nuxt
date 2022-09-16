42f222c10629b5d0ad727f0792ceb919e1c28f33
Victor Kwok <victorkwok97@gmail.com>
2022-09-16 18:14:13 +0800

    let everyone use e commerce

--- composables/useIsSubscribed.ts
+++ composables/useIsSubscribed.ts
@@ -3,11 +3,12 @@ export default function useIsSubscribed() {
     const igUsername = useIgUsername()
 
     const isSubscribed = computed(() => {
-        return [
-            "myyororoom_",
-            "shoperuse.tester"
-            // "valentim.manas.cs"
-        ].includes(igUsername.value)
+        // return [
+        //     "myyororoom_",
+        //     "shoperuse.tester"
+        //     // "valentim.manas.cs"
+        // ].includes(igUsername.value)
+        return true
     })
 
     return {
