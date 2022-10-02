9b439c061b7b1d0007a918d1e2d34e63038c11fe
DESKTOP-62LBCR7\Victor <victorkwok97@gmail.com>
2022-10-02 23:22:57 +0800

    wip: e-commerce promo

--- pages/my/e-commerce.vue
+++ pages/my/e-commerce.vue
@@ -1,176 +1,176 @@
 <template>
   <div>
     <!-- First time set up -->
-    <div v-if="!commerceData" class="info-group !pb-8">
+    <template v-if="!commerceData">
+      <template v-if="step == Step.INIT">
 
-      <!-- Stepper -->
-      <div v-if="step !== Step.INIT && step !== Step.COMPLETE" class="flex items-center mb-4">
-        <div v-for="(s, i) in steps.slice(1, steps.length - 1)"
-             :key="s"
-             class="h-1 mr-2"
-             :class="currentStepIndex > i ? 'bg-pink-400' : 'bg-gray-200'"
-             :style="'width: ' + (100 / (steps.length - 2)) + '%'"></div>
-        <div class="text-xs text-gray-500">{{ currentStepIndex }}/{{ steps.length - 2 }}</div>
-      </div>
-
-      <!-- Loading -->
-      <div v-if="savingConfig" class="text-center py-16">
-        <i class="spr-spin4 animate-spin text-6xl text-pink-400"></i>
-        <div class="mt-4 text-2xl text-pink-700 font-semibold">
-          設定中
+        <div class="grid grid-cols-3">
+          <div v-for="(point, i) in points"
+               data-aos="fade-up"
+               data-aos-anchor-placement="top-bottom"
+               data-aos-duration="1000"
+               class="col-span-1 rounded bg-white md:text-center p-4">
+            <div class="text-pink-400 text-2xl font-semibold">{{point.title}}</div>
+            <!--                <div class="mt-4">{{point.subtitle}}</div>-->
+            <div class="mt-4 text-sm">{{point.content}}</div>
+          </div>
         </div>
+
+        <div class="flex">
+          <div class="w-1/2">
+
+          </div>
+          <div class="w-1/2">
+            <div class="text-pink-400 font-bold">"迎合你的推廣需要"</div>
+            <div class="text-2xl md:text-4xl font-bold mt-1">隨意設定折扣</div>
+            <div>你可以在我們的系統上設定折扣類型、折扣條件、折扣期限等等。讓你可以隨心所欲地推廣你的產品。</div>
+          </div>
+        </div>
+        <div class="flex">
+          <div class="w-1/2">
+            <div class="text-pink-400 font-bold">""</div>
+            <div class="text-2xl md:text-4xl font-bold mt-1">方便郵寄</div>
+            <div>内置順豐站、順便智能櫃地點，讓顧客可以輕鬆選擇。同時支援面交、到付等郵寄設定。</div>
+          </div>
+          <div class="w-1/2">
+
+          </div>
+        </div>
+        <div class="flex">
+          <div class="w-1/2">
+
+          </div>
+          <div class="w-1/2">
+            <div class="text-pink-400 font-bold">""</div>
+            <div class="text-2xl md:text-4xl font-bold mt-1">付款方法</div>
+            <div></div>
+          </div>
+        </div>
+
+
+
+        <div class="text-xl mt-4">
+          設定你的網店後，顧客便可直接在Shoperuse下單購買你的產品！
+        </div>
+        <div class="mt-2 text-gray-600">
+          首次設定時，我們將會逐步指引你設定。設定後，所有資料也可隨時再次修改。
+        </div>
+        <button class="mt-4 btn-primary" @click="incrementStep">立即開始設定</button>
+
+
+
+      </template>
+      <div v-else class="info-group !pb-8">
+
+        <!-- Stepper -->
+        <div v-if="step !== Step.INIT && step !== Step.COMPLETE" class="flex items-center mb-4">
+          <div v-for="(s, i) in steps.slice(1, steps.length - 1)"
+               :key="s"
+               class="h-1 mr-2"
+               :class="currentStepIndex > i ? 'bg-pink-400' : 'bg-gray-200'"
+               :style="'width: ' + (100 / (steps.length - 2)) + '%'"></div>
+          <div class="text-xs text-gray-500">{{ currentStepIndex }}/{{ steps.length - 2 }}</div>
+        </div>
+
+        <!-- Loading -->
+        <div v-if="savingConfig" class="text-center py-16">
+          <i class="spr-spin4 animate-spin text-6xl text-pink-400"></i>
+          <div class="mt-4 text-2xl text-pink-700 font-semibold">
+            設定中
+          </div>
+        </div>
+
+        <!-- Edit content -->
+        <div v-else class="text-center w-full md:w-3/5 mx-auto">
+
+          <template v-if="step == Step.MAILING">
+            <div class="text-2xl">
+              郵寄方法
+            </div>
+            <div class="text-gray-500">
+              免郵優惠（e.g. 滿3件免郵）將在稍後步驟填寫，此步驟只需填寫郵寄原價。
+            </div>
+
+            <LazyMailingEditor class="mt-4" v-model="configCommerceData.mailing" :allowRemoveAll="true"></LazyMailingEditor>
+
+            <button class="mt-4 btn-primary" :disabled="configCommerceData.mailing.length === 0" @click="incrementStep">下一步</button>
+          </template>
+          <template v-else-if="step == Step.PAYMENT">
+            <div class="text-2xl">
+              付款方法
+            </div>
+            <div class="text-gray-500">
+              顧客將會直接付款給你，所有款項皆不會經過Shoperuse。
+            </div>
+
+            <LazyPaymentEditor class="mt-4" v-model="configCommerceData" :allowRemoveAll="true"></LazyPaymentEditor>
+
+            <div class="flex justify-between">
+              <button class="mt-4 mr-4 btn-outline" @click="decrementStep">上一步</button>
+              <button class="mt-4 btn-primary" :disabled="configCommerceData.paymentMethodData.length === 0" @click="incrementStep">下一步</button>
+            </div>
+
+          </template>
+          <template v-else-if="step == Step.DISCOUNT">
+            <div class="text-2xl">
+              優惠設定
+            </div>
+
+            <div class="flex justify-between items-center">
+              <div class="text-left text-xl font-semibold">
+                店鋪折扣優惠
+              </div>
+              <lazy-basic-toggle v-model="hasDiscount"></lazy-basic-toggle>
+            </div>
+            <div class="text-left text-gray-500">
+              此優惠適用於你的店鋪内所有的產品。如果只想為特定產品設定優惠，請在「我的貼文」設定。
+            </div>
+
+            <LazyDiscountEditor class="mt-4" v-if="hasDiscount" v-model="tempDiscount"></LazyDiscountEditor>
+
+            <hr class="my-4"/>
+
+            <div class="flex justify-between items-center">
+              <div class="text-left text-xl font-semibold">
+                免郵優惠
+              </div>
+              <lazy-basic-toggle v-model="hasMailingDiscount"></lazy-basic-toggle>
+            </div>
+            <div class="text-left text-gray-500">
+              在指定條件下免除郵費。
+            </div>
+
+            <LazyMailingDiscountEditor v-if="hasMailingDiscount" class="mt-4" v-model="tempMailingDiscount"></LazyMailingDiscountEditor>
+
+            <div class="flex justify-between">
+              <button class="mt-4 mr-4 btn-outline" @click="decrementStep">上一步</button>
+              <button class="mt-4 btn-primary" @click="incrementStep">完成</button>
+            </div>
+
+          </template>
+          <template v-else-if="step == Step.COMPLETE">
+            <div>
+              <i class="spr-ok text-6xl text-pink-700"></i>
+              <div class="mt-2 text-2xl text-pink-700 font-semibold">
+                設定完成
+              </div>
+            </div>
+            <div class="mt-4">
+              恭喜，顧客現在可直接在Shoperuse上下單購買你的產品！
+            </div>
+            <div class="mt-2">
+              所有設定可於「網店設定」隨時修改
+            </div>
+            <div class="flex justify-center">
+              <button class="mt-4 mr-4 btn-outline" @click="navigateToCommerceSettings">返回網店設定</button>
+              <button class="mt-4 btn-primary" @click="navigateToOrderList">查看訂單列表</button>
+            </div>
+          </template>
+
+        </div>
+
       </div>
-
-      <!-- Edit content -->
-      <div v-else class="text-center w-full md:w-3/5 mx-auto">
-
-        <template v-if="step == Step.INIT">
-
-          <div class="grid grid-cols-3">
-            <div v-for="(point, i) in points"
-                 data-aos="fade-up"
-                 data-aos-anchor-placement="top-bottom"
-                 data-aos-duration="1000"
-                 class="col-span-1 rounded bg-white md:text-center p-4">
-              <div class="text-pink-400 text-2xl font-semibold">{{point.title}}</div>
-              <!--                <div class="mt-4">{{point.subtitle}}</div>-->
-              <div class="mt-4 text-sm">{{point.content}}</div>
-            </div>
-          </div>
-
-
-
-          <div>
-            <div class="w-1/2">
-
-            </div>
-            <div class="w-1/2">
-              <div>"迎合你的推廣需要"</div>
-              <div>隨意設定折扣</div>
-              <div>你可以在我們的系統上設定折扣類型、折扣條件、折扣期限等等。讓你可以隨心所欲地推廣你的產品。</div>
-            </div>
-          </div>
-          <div>
-            <div class="w-1/2">
-              <div>""</div>
-              <div>郵寄方法</div>
-              <div></div>
-            </div>
-            <div class="w-1/2">
-
-            </div>
-          </div>
-          <div>
-            <div class="w-1/2">
-
-            </div>
-            <div class="w-1/2">
-              <div>""</div>
-              <div>付款方法</div>
-              <div></div>
-            </div>
-          </div>
-
-
-
-          <div class="text-xl mt-4">
-            設定你的網店後，顧客便可直接在Shoperuse下單購買你的產品！
-          </div>
-          <div class="mt-2 text-gray-600">
-            首次設定時，我們將會逐步指引你設定。設定後，所有資料也可隨時再次修改。
-          </div>
-          <button class="mt-4 btn-primary" @click="incrementStep">立即開始設定</button>
-
-
-
-        </template>
-        <template v-else-if="step == Step.MAILING">
-          <div class="text-2xl">
-            郵寄方法
-          </div>
-          <div class="text-gray-500">
-            免郵優惠（e.g. 滿3件免郵）將在稍後步驟填寫，此步驟只需填寫郵寄原價。
-          </div>
-
-          <LazyMailingEditor class="mt-4" v-model="configCommerceData.mailing" :allowRemoveAll="true"></LazyMailingEditor>
-
-          <button class="mt-4 btn-primary" :disabled="configCommerceData.mailing.length === 0" @click="incrementStep">下一步</button>
-        </template>
-        <template v-else-if="step == Step.PAYMENT">
-          <div class="text-2xl">
-            付款方法
-          </div>
-          <div class="text-gray-500">
-            顧客將會直接付款給你，所有款項皆不會經過Shoperuse。
-          </div>
-
-          <LazyPaymentEditor class="mt-4" v-model="configCommerceData" :allowRemoveAll="true"></LazyPaymentEditor>
-
-          <div class="flex justify-between">
-            <button class="mt-4 mr-4 btn-outline" @click="decrementStep">上一步</button>
-            <button class="mt-4 btn-primary" :disabled="configCommerceData.paymentMethodData.length === 0" @click="incrementStep">下一步</button>
-          </div>
-
-        </template>
-        <template v-else-if="step == Step.DISCOUNT">
-          <div class="text-2xl">
-            優惠設定
-          </div>
-
-          <div class="flex justify-between items-center">
-            <div class="text-left text-xl font-semibold">
-              店鋪折扣優惠
-            </div>
-            <lazy-basic-toggle v-model="hasDiscount"></lazy-basic-toggle>
-          </div>
-          <div class="text-left text-gray-500">
-            此優惠適用於你的店鋪内所有的產品。如果只想為特定產品設定優惠，請在「我的貼文」設定。
-          </div>
-
-          <LazyDiscountEditor class="mt-4" v-if="hasDiscount" v-model="tempDiscount"></LazyDiscountEditor>
-
-          <hr class="my-4"/>
-
-          <div class="flex justify-between items-center">
-            <div class="text-left text-xl font-semibold">
-              免郵優惠
-            </div>
-            <lazy-basic-toggle v-model="hasMailingDiscount"></lazy-basic-toggle>
-          </div>
-          <div class="text-left text-gray-500">
-            在指定條件下免除郵費。
-          </div>
-
-          <LazyMailingDiscountEditor v-if="hasMailingDiscount" class="mt-4" v-model="tempMailingDiscount"></LazyMailingDiscountEditor>
-
-          <div class="flex justify-between">
-            <button class="mt-4 mr-4 btn-outline" @click="decrementStep">上一步</button>
-            <button class="mt-4 btn-primary" @click="incrementStep">完成</button>
-          </div>
-
-        </template>
-        <template v-else-if="step == Step.COMPLETE">
-          <div>
-            <i class="spr-ok text-6xl text-pink-700"></i>
-            <div class="mt-2 text-2xl text-pink-700 font-semibold">
-              設定完成
-            </div>
-          </div>
-          <div class="mt-4">
-            恭喜，顧客現在可直接在Shoperuse上下單購買你的產品！
-          </div>
-          <div class="mt-2">
-            所有設定可於「網店設定」隨時修改
-          </div>
-          <div class="flex justify-center">
-            <button class="mt-4 mr-4 btn-outline" @click="navigateToCommerceSettings">返回網店設定</button>
-            <button class="mt-4 btn-primary" @click="navigateToOrderList">查看訂單列表</button>
-          </div>
-        </template>
-
-      </div>
-
-    </div>
+    </template>
 
     <div v-else>
       <div class="info-group">
